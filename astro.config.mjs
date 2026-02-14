a9af8508aaf0febe47cfdaeb591eab56fbe6047b
Zach Auten <zja@zach.sexy>
2026-02-14 00:52:05 -0500

    try wildcard exclude

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     },
     routes: {
       extend: {
-        exclude: [{ pattern: "/api/auth" }, { pattern: "/api/callback" }]
+        exclude: [{ pattern: "/api/*" }]
       }
     }
   }),
