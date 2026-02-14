dc525cb741638720683acc2850b632318ae84ce4
Zach Auten <zja@zach.sexy>
2026-02-14 00:11:06 -0500

    add assetignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
